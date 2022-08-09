b5eaa2c9fbe9e07d4cd565ed5161fdbc24348793
ilham fathullah <ilhamfathullah04@gmail.com>
2022-08-09 10:54:08 +0700

    update

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
 // import fmt, net/http, github.com/gorilla/mux here ...
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 // Create main function to show "hello world" here ...
+
+func main() {
+	fmt.Println("Hello world")
+
+	route := mux.NewRouter()
+	// r.GET("/", (req, res)=> { })
+	route.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello World"))
+	}).Methods("GET")
+
+	http.ListenAndServe("localhost:5000", route)
+}
