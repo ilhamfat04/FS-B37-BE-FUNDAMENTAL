40c840916f8f92d8494b926b43ed261a84e1030b
ilham fathullah <ilhamfathullah04@gmail.com>
2022-08-09 14:42:54 +0700

    update

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	// Import dumbmerch/database here ...
-	// Import dumbmerch/pkg/mysql here ...
+	"dumbmerch/database"
+	"dumbmerch/pkg/mysql"
 	"dumbmerch/routes"
 	"fmt"
 	"net/http"
@@ -12,9 +12,11 @@ import (
 
 func main() {
 	// initial DB here ...
+	mysql.DatabaseInit()
 
-	// Run migration here ...
-	
+	// run migration
+	database.RunMigration()
+
 	r := mux.NewRouter()
 
 	routes.RouteInit(r.PathPrefix("/api/v1").Subrouter())
@@ -22,4 +24,3 @@ func main() {
 	fmt.Println("server running localhost:5000")
 	http.ListenAndServe("localhost:5000", r)
 }
-
